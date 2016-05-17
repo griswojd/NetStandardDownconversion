e83e3ad0b265cb7a0ede638b059f51da355c273f
Jeff Griswold <jeffrey.d.griswold@gmail.com>
2016-05-17 12:18:44 -0500

    Simple app with dependency on NetStandard1.1

---
+++ NetStandardDownconversion/NetStandardDownconversion/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetStandardDownconversion
+{
+	public class Class1
+	{
+		public void DoWork()
+		{
+			var services = new ServiceCollection();
+			var provider = services.BuildServiceProvider();
+		}
+	}
+}
